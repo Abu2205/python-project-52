a73ed6e38cc33584b7966c053fbef13e1b5f70f4
Abu <abylaikhanturmanov@gmail.com>
2025-06-16 15:23:44 +0000

    complete step7

--- task_manager_app/views.py
+++ task_manager_app/views.py
@@ -18,6 +18,16 @@ class IndexView(TemplateView):
     """Главная страница с приветствием"""
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # ВРЕМЕННО: Тест для Rollbar
+        if self.request.GET.get('test_rollbar'):
+            a = None
+            a.hello()  # Создаем ошибку для тестирования
+            
+        return context
+
 
 # ========== USER VIEWS ==========
 
